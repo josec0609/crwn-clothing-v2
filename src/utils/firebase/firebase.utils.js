656c283d36072597e65f8e797b834e3417b36dcf
Jose Ceballos <josec0609@gmail.com>
2022-04-24 14:34:29 -0700

    added sign-in component along with firebase library and utilities

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,55 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
+import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyDhIu8hHX8oMsa7YX0uXuZfzzH-817uS7E',
+  authDomain: 'crwn-clothing-db-13031.firebaseapp.com',
+  projectId: 'crwn-clothing-db-13031',
+  storageBucket: 'crwn-clothing-db-13031.appspot.com',
+  messagingSenderId: '1096439144328',
+  appId: '1:1096439144328:web:7ec9586a90cd98ed74e43d',
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+const googleAuthProvider = new GoogleAuthProvider();
+
+googleAuthProvider.setCustomParameters({
+  prompt: 'select_account',
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, googleAuthProvider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  // Check if collection/document exists in DB, returns document ref regardless if collection or document exist. (i.e. google will return a valid ref even if objects dont exist in DB since Google has necessary info from call to return a valid ref that can be used to be able to set document data to collection/document that might not yet exist)
+  const userDocRef = doc(db, 'users', userAuth.uid);
+
+  console.log(userDocRef);
+
+  const userSnapshot = await getDoc(userDocRef);
+  console.log(userSnapshot);
+  console.log(userSnapshot.exists());
+
+  // if user does not exist
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+      });
+    } catch (error) {
+      console.log('error creating the user', error.message);
+    }
+  }
+  return userDocRef;
+};
