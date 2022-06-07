b8a3b383da817690043b2a43bdbac764457e1824
Jose Ceballos <josec0609@gmail.com>
2022-06-07 01:41:19 -0700

    refactor rename signin component to auth, add styling to auth component to evently space sign in and sign up forms

--- src/routes/authentication/authentication.component.jsx
+++ src/routes/authentication/authentication.component.jsx
@@ -1,13 +1,15 @@
 import SignInForm from '../../components/sign-in-form/sign-in-form.component';
 import SignUpForm from '../../components/sign-up-form/sign-up-form.component';
 
-const SignIn = () => {
+import './authentication.styles.scss';
+
+const Authentication = () => {
   return (
-    <div>
+    <div className="authentication-container">
       <SignInForm />
       <SignUpForm />
     </div>
   );
 };
 
-export default SignIn;
+export default Authentication;
