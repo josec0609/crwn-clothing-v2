971f4e755895a4d9476147599afd1ecd18393582
Jose Ceballos <josec0609@gmail.com>
2022-06-08 01:06:28 -0700

    Fix syntax issue

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import UserProvider from './contexts/user.context';
+import { UserProvider } from './contexts/user.context';
 
 ReactDOM.render(
   <React.StrictMode>
