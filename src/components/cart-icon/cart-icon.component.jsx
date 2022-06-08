6c776ee2630b4f3adba5e076368af671bb850f9c
Jose Ceballos <josec0609@gmail.com>
2022-06-08 02:38:24 -0700

    created cart icon, & cart dropdown components, added both components to navigation bar component, created cart context to control opening cart dropdown onClick of cart icon

---
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -0,0 +1,20 @@
+import { useContext } from 'react';
+import { ReactComponent as ShoppingBagIcon } from '../../assets/shopping-bag.svg';
+import { CartContext } from '../../contexts/cart.context';
+
+import './cart-icon.styles.scss';
+
+const CartIcon = () => {
+  const { isCartOpen, setIsCartOpen } = useContext(CartContext);
+
+  const toggleIsCartOpen = () => setIsCartOpen(!isCartOpen);
+
+  return (
+    <div className="cart-icon-container" onClick={toggleIsCartOpen}>
+      <ShoppingBagIcon className="shopping-icon" />
+      <span className="item-count">0</span>
+    </div>
+  );
+};
+
+export default CartIcon;
