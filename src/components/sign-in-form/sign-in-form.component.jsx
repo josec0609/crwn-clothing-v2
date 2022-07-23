2f328a11a6decc6c293f0525db2b6d59b7f0da8e
Jose Ceballos <josec0609@gmail.com>
2022-07-23 16:24:14 -0700

    migrate button to styled components, utilize base stlyed component pattern creating baseButton component that will be extended to other button components in app

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -4,7 +4,7 @@ import {
   signInWithGooglePopup,
 } from '../../utils/firebase/firebase.utils';
 import FormInput from '../form-input/form-input.component';
-import Button from '../button/button.component';
+import Button, { BUTTON_TYPE_CLASSES } from '../button/button.component';
 import './sign-in-form.styles.scss';
 
 const defaultFormFields = {
@@ -74,7 +74,7 @@ const SignInForm = () => {
         />
         <div className="buttons-container">
           <Button type="submit">Sign in</Button>
-          <Button type="button" buttonType="google" onClick={signInGoogleUser}>
+          <Button type="button" buttonType={BUTTON_TYPE_CLASSES.google} onClick={signInGoogleUser}>
             Google Sign In
           </Button>
         </div>
