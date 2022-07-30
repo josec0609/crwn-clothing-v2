8ea9959c3129e6de7dd9dd96e24ee57b1cf00f66
Jose Ceballos <josec0609@gmail.com>
2022-07-29 22:35:24 -0700

    add stripe utils folder, wire up stripe in index.js to be able to create stripe components

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { Elements } from '@stripe/react-stripe-js';
 
 import './index.scss';
 import App from './App';
@@ -8,6 +9,7 @@ import reportWebVitals from './reportWebVitals';
 import { UserProvider } from './contexts/user.context';
 import { CategoriesProvider } from './contexts/categories.context';
 import { CartProvider } from './contexts/cart.context';
+import { stripePromise } from './utils/stripe/stripe.utils';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -15,7 +17,9 @@ ReactDOM.render(
       <UserProvider>
         <CategoriesProvider>
           <CartProvider>
-            <App />
+            <Elements stripe={stripePromise}>
+              <App />
+            </Elements>
           </CartProvider>
         </CategoriesProvider>
       </UserProvider>
